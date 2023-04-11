ff52a0aa57d053a602af4a1dcc9a4fd5e0349ead
Caitlin Cradick <caitlincradick@gmail.com>
2023-04-11 14:37:39 -0700

    Add functionality to randomize hex code

---
+++ main.js
@@ -0,0 +1,34 @@
+//QUERY SELECTORS
+var newPaletteButton = document.querySelector('.new-button');
+
+
+
+
+//EVENT LISTENERS
+// window.addEventListener(generateHexCode);
+newPaletteButton.addEventListener('click', generateHexCode);
+
+
+
+//GLOBAL VARIABLES
+var hexCharacters = ['A','B','C','D','E','F','0','1','2','3','4','5','6','7','8','9'];
+var hexCodes = [];
+
+
+//JS 
+//DATA MODEL- anything that is stored
+function generateRandomIndex(array) {
+    return Math.floor(Math.random() * array.length);
+}
+
+function generateHexCode(){
+    if(hexCodes.length < 6 ){
+        var getRandomCharacter = hexCharacters[generateRandomIndex(hexCharacters)]
+         hexCodes.push(getRandomCharacter)
+    }
+}
+
+
+//DOM- INNERHTML/TEXT, anything we see on the page 
+
+
