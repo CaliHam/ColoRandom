f90b5a0ab264947ae725e675cf6d33f1977b0d43
Caitlin Cradick <caitlincradick@gmail.com>
2023-04-11 14:59:09 -0700

    Add method to make the array a string

--- main.js
+++ main.js
@@ -13,6 +13,7 @@ newPaletteButton.addEventListener('click', generateHexCode);
 //GLOBAL VARIABLES
 var hexCharacters = ['A','B','C','D','E','F','0','1','2','3','4','5','6','7','8','9'];
 var hexCodes = [];
+var hexCodeString;
 
 
 //JS 
@@ -22,10 +23,14 @@ function generateRandomIndex(array) {
 }
 
 function generateHexCode(){
-    if(hexCodes.length < 6 ){
+    for(var i = 0; i < hexCharacters.length; i++){
+      if(hexCodes.length < 6 ){
         var getRandomCharacter = hexCharacters[generateRandomIndex(hexCharacters)]
          hexCodes.push(getRandomCharacter)
+        }
     }
+    hexCodeString = hexCodes.join('')
+    return hexCodeString
 }
 
 
