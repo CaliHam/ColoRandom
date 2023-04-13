bd2117f3cda288fee9016b30107b93ebde901296
Paulina Rubio-Parra <paulinaisabel@me.com>
2023-04-12 21:26:56 -0400

    Refactored the current palette functionality.

--- main.js
+++ main.js
@@ -1,30 +1,56 @@
 //QUERY SELECTORS
 var newPaletteButton = document.querySelector('.new-button');
-var hex1 = document.querySelector('#hex1');
-var hex2 = document.querySelector('#hex2');
-var hex3 = document.querySelector('#hex3');
-var hex4 = document.querySelector('#hex4');
-var hex5 = document.querySelector('#hex5');
-var allHex = document.querySelectorAll('.hexvalue')
-var hex1color = document.querySelector('#color1')
-var hex2color = document.querySelector('#color2')
-var hex3color = document.querySelector('#color3')
-var hex4color = document.querySelector('#color4')
-var hex5color = document.querySelector('#color5')
+var lockButton = document.querySelectorAll('.lock')
+var unlockButton = document.querySelectorAll('.unlock')
+var hexContainers = document.querySelectorAll('.hex-containers')
 
 //EVENT LISTENERS
 window.addEventListener("load", function(event){
     event.preventDefault();
-    displayHexCodes()
+    generatePalette(currentPalette)
+    displayHexCodes()    
 });
-newPaletteButton.addEventListener('click', displayHexCodes);
 
+newPaletteButton.addEventListener('click', function() {
+    generatePalette(currentPalette)
+    displayHexCodes()    
+});
+
+for (let i = 0; i < unlockButton.length; i++) {
+    unlockButton[i].addEventListener("click", function() {
+      unlockButton[i].classList.toggle("unlock");
+      unlockButton[i].classList.toggle("lock");
+    });
+}
 
 
 //GLOBAL VARIABLES
 var hexCharacters = ['A','B','C','D','E','F','0','1','2','3','4','5','6','7','8','9'];
 var hexCodeString;
 var allCodes = [];
+var currentPalette = {
+    hexColors: [{
+        hexCode: '#EA9999',
+        isLocked: false
+    },
+    {
+        hexCode: '#FACB9C',
+        isLocked: false
+    },
+    {
+        hexCode: '#FFE59A',
+        isLocked: false
+    },
+    {
+        hexCode: '#B6D7A8',
+        isLocked: false
+    },
+    {
+        hexCode: '#A4C4CA',
+        isLocked: false
+    }],
+    id: Date.now()
+}
 
 
 //JS 
@@ -45,20 +71,27 @@ function generateHexCode(){
 
 
 //DOM- INNERHTML/TEXT, anything we see on the page 
-function displayHexCodes(){
-    var codeStrings = [];
-    for(var i = 0; i < 5; i++){
-        codeStrings.push(generateHexCode())
+function generatePalette(currentPalette) {
+    console.log(currentPalette.hexColors.isLocked)
+    for (var i = 0; i < currentPalette.hexColors.length; i++){
+        if (!currentPalette.hexColors[i].isLocked) {
+            currentPalette.hexColors[i].hexCode = generateHexCode()
         }
-        hex1.innerText = codeStrings[0];
-        hex1color.style.backgroundColor = codeStrings[0];
-        hex2.innerText = codeStrings[1];
-        hex2color.style.backgroundColor = codeStrings[1];
-        hex3.innerText = codeStrings[2];
-        hex3color.style.backgroundColor = codeStrings[2];
-        hex4.innerText = codeStrings[3];
-        hex4color.style.backgroundColor = codeStrings[3];
-        hex5.innerText = codeStrings[4];
-        hex5color.style.backgroundColor = codeStrings[4];
-        allCodes = codeStrings;
+    }
+    console.log(currentPalette)  
+    return currentPalette
+}
+
+function displayHexCodes(){
+    hexContainers.forEach((hex, index) => {
+        var box = hex.querySelector('.box')
+        var hexValue = hex.querySelector('.hexvalue')
+        box.style.backgroundColor = currentPalette.hexColors[index].hexCode
+        hexValue.innerText = currentPalette.hexColors[index].hexCode
+    })
+}
+
+function toggleLocks(event) {
+    event.target.classList.toggle('unlock');
+    event.target.classList.toggle('lock');
 }
